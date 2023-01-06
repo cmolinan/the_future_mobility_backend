5b197395cae03aa567d650eb474d4051d4a6e154
wandergithub <wandergonzalez0@hotmail.com>
2023-01-06 16:57:28 -0400

    Update routes methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :vehicles, only: [:index, :create, :show, :delete]
-      resources :reservations, only: [:index, :create, :delete]
+      resources :vehicles, only: [:index, :create, :show, :destroy]
+      resources :reservations, only: [:index, :create, :destroy]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
