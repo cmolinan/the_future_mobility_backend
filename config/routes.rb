443cbc72634943a9ab23ca5910f21f02c8959028
wandergithub <wandergonzalez0@hotmail.com>
2023-01-06 16:49:37 -0400

    Update routes to add versioned endpoint and shorter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/show'
-  get 'reservations/create'
-  get 'vehicles/index'
-  get 'vehicles/show'
-  get 'vehicles/create'
-  get 'vehicles/destroy'
-  get 'users/index'
+  namespace :api do
+    namespace :v1 do
+      resources :vehicles, only: [:index, :create, :show, :delete]
+      resources :reservations, only: [:index, :create, :delete]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
