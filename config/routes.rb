76913cb81b62414be7a854386ddb2bc125ecac0b
cmolinan <cmolinan@gmail.com>
2023-01-07 00:00:34 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
-
 Rails.application.routes.draw do
   devise_for :users, defaults: { format: :json } 
-  get 'reservations/index'
-  get 'reservations/show'
-  get 'reservations/create'
-  get 'vehicles/index'
-  get 'vehicles/show'
-  get 'vehicles/create'
-  get 'vehicles/destroy'
-  get 'users/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :vehicles
+  resources :users, only: %i[index] do
+    resources :reservations, only: %i[index show create]
+  end
+
+  post 'users/login' => 'users#login'
+  post 'users/signup' => 'users#signup'
+
 end
