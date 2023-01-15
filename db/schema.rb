84790156f9a319ec541e38667f820e82a0cacc7e
cmolinan <cmolinan@gmail.com>
2023-01-15 11:07:07 -0500

    Add three more columns to Vehicle table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_155444) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_160356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_155444) do
     t.string "enginetourque"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "transmission"
+    t.string "fuel_capacity"
+    t.string "seat"
   end
 
   add_foreign_key "reservations", "users", on_delete: :cascade
